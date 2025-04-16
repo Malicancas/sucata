b65fba60a24e6dac17d37d9da0f9edd2dff8950b
malicancas <josecraveiro544@gmail.com>
2025-04-16 23:06:53 +0100

    Add joystick configuration and launch file for teleoperation

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -23,7 +23,7 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'false', 'use_ros2_control': 'true'}.items()
     )
 
     default_world = os.path.join(
@@ -78,6 +78,8 @@ def generate_launch_description():
             f'config_file:={bridge_params}',
         ]
     )
+
+
 
     return LaunchDescription([
         rsp,
